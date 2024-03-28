60f666717ccfbec5c2982c0ebb5f233d6fd2d65b
Marcelluki <marcell.rangel29@gmail.com>
2024-03-27 19:16:19 -0700

    added handling for when incorrect route is requested

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const { getUsers, createUser, getUser } = require("../controllers/users");
+const { NOT_FOUND_ERROR } = require("../utils/errors");
 
 router.get("/", getUsers);
 router.get("/:userId", getUser);
 router.post("/", createUser);
-
+router.use((req, res) => {
+  res.status(NOT_FOUND_ERROR).send({ message: "Route not Found" });
+});
 module.exports = router;
