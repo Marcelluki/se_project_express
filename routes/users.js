e5b1876eed045162cfac7eed5d776e055d6c49d7
Marcelluki <marcell.rangel29@gmail.com>
2024-03-27 19:26:39 -0700

    removed handling or nonexisting route from user.js. it only goes inside of index.js to handle all Routes

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,4 @@ const { NOT_FOUND_ERROR } = require("../utils/errors");
 router.get("/", getUsers);
 router.get("/:userId", getUser);
 router.post("/", createUser);
-router.use((req, res) => {
-  res.status(NOT_FOUND_ERROR).send({ message: "Route not Found" });
-});
 module.exports = router;
