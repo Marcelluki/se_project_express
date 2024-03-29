179468d23e27d5b73bd061b320c7224effecdd9e
Marcelluki <marcell.rangel29@gmail.com>
2024-03-28 20:16:30 -0700

    fixed small eslint error

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const { getUsers, createUser, getUser } = require("../controllers/users");
-const { NOT_FOUND_ERROR } = require("../utils/errors");
 
 router.get("/", getUsers);
 router.get("/:userId", getUser);
