beded1356ac79e47e0b5744f6675e8cbe00b7393
Marcelluki <marcell.rangel29@gmail.com>
2024-06-23 16:49:50 -0700

    added validate login and validate user to post of signin and signup routes

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,12 @@ const { NOT_FOUND_ERROR, NotFoundError } = require("../utils/errors");
 const clothingItemRouter = require("./clothingItem");
 const userRouter = require("./users");
 const authorizationMiddleware = require("../middlewares/auth");
+const { validateLogin, validateUser } = require("../middlewares/validation");
 
 router.use("/users", authorizationMiddleware, userRouter);
 router.use("/items", clothingItemRouter);
-router.post("/signin", login);
-router.post("/signup", createUser);
+router.post("/signin", validateLogin, login);
+router.post("/signup", validateUser, createUser);
 router.use((req, res, next) => {
   // res.status(NOT_FOUND_ERROR).send({ message: "Route not Found" });
   next(new NotFoundError("Route Not Found"));
